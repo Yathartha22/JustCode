9f0ce4118e221376b312021cadafd6b66ab77294
Ashutoshu11 <ashutosh.sanyal11@gmail.com>
2020-10-30 11:51:18 +0530

    Update and rename Job Sequencing Problem to JobSequencingProblem.cpp

--- Greedy Algorithms/JobSequencingProblem.cpp
+++ Greedy Algorithms/JobSequencingProblem.cpp
@@ -1,32 +1,5 @@
-Given an array of jobs where every job has a deadline and associated profit if the job is finished before the
-deadline. It is also given that every job takes single unit of time, so the minimum possible deadline for any
-job is 1. How to maximize total profit if only one job can be scheduled at a time.
-Examples:
-Input: Four Jobs with following deadlines and profits
-JobID Deadline Profit
-  a       4       20
-  b       1       10
-  c       1       40
-  d       1       30
-Output: Following is maximum profit sequence of jobs
-c, a
-
-1) Sort all jobs in decreasing order of profit.
-2) Initialize the result sequence as first job in sorted jobs.
-3) Do following for remaining n-1 jobs
-.......a) If the current job can fit in the current result sequence
-          without missing the deadline, add current job to the result.
-          Else ignore the current job.
-          
-          
-The Following is C++ implementation of above algorithm.
-
-// Program to find the maximum profit job sequence from a given array
-// of jobs with deadlines and profits
-
-
 #include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 // A structure to represent a job
 struct Job
@@ -45,8 +18,10 @@ void printJobScheduling(Job arr[], int n)
 {
       // Sort all jobs according to decreasing order of prfit
       sort(arr, arr+n, comparison);
+      
       int result[n]; // To store result (Sequence of jobs)
       bool slot[n]; // To keep track of free time slots
+      
       // Initialize all slots to be free
       for (int i=0; i<n; i++)
         slot[i] = false;
